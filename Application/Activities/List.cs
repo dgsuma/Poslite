ef98ef91692b8d72f1f8ab710e3ba927f93bf884
Duminda <duminda@example.com>
2020-07-01 07:55:42 +0530

    End of Section 4

---
+++ Application/Activities/List.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            { 
+                _context = context;
+            }
+
+            // Using CancellationToken
+            // private readonly ILogger<List> _logger;
+            // public Handler(DataContext context, ILogger<List> logger)
+            // {
+            //     _logger = logger;
+            //     _context = context;
+            // }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activities = await _context.Activities.ToListAsync(cancellationToken);
+
+                return activities;
+            }
+
+            // Using CancellationToken
+            // public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            // {
+            //     try
+            //     {
+            //         for (var i = 0; i < 10; i++)
+            //         {
+            //             cancellationToken.ThrowIfCancellationRequested();
+            //             await Task.Delay(1000, cancellationToken);
+            //             _logger.LogInformation($"Task {i} has completed");
+            //         }
+            //     }
+            //     catch (Exception ex) when (ex is TaskCanceledException)
+            //     {
+            //         _logger.LogInformation("Task was cancelled");
+            //     }
+
+            //     var activities = await _context.Activities.ToListAsync(cancellationToken);
+
+            //     return activities;
+            // }
+        }
+    }
+}
